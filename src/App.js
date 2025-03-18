fac75346e5c82b95f8911d2575c86461ab81cb96
taewoo01 <xodn9402@naver.com>
2025-03-18 13:24:10 +0900

    기본 폴더 설정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-3xl font-bold p-4">
+      수화 동작 인식 모바일 시스템
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // 📌 JSX 파일 반영
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
